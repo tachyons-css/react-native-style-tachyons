5b350e012c324f2012ea88591a9663f535a3883e
Stefan N <stefan@flitz.ch>
2017-01-31 22:50:06 +0100

    Fixed styling of JSX

--- test/index_spec.js
+++ test/index_spec.js
@@ -195,8 +195,21 @@ test("wrapping", t => {
 });
 
 test("wrapping JSX", t => {
-    const orig = (<div cls="b">Some Text</div>);
-    const expexted = (<div cls="b" style={[{ fontWeight: 'bold' }]}>Some Text</div>);
+    const orig = (
+        <div 
+            cls="b"
+        >
+            Some Text
+        </div>
+    );
+    const expexted = (
+        <div 
+            cls="b"
+            style={[{ fontWeight: 'bold' }]}
+        >
+            Some Text
+        </div>
+    );
 
     const wrapped = wrap(orig);
     t.deepEqual(wrapped, expexted, "wrapped as expected");
