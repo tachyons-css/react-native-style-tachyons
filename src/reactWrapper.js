2204287a41280bd6524d21d8dccc1100356b0e59
Stefan N <stefnnn@users.noreply.github.com>
2017-02-04 11:32:33 +0100

    Enable wrapping of JSX statements (#31)
    * Enable wrapping of JSX statements
    
    For some components, namely ListView and its renderHeader / renderFooter / renderSeparator properties, will not be styled properly with the class wrapper. With this PR, such methods can be wrapped like this:
    
    ```javascript
    const renderHeader = NativeTachyons.wrap(<Text cls="b">Some Text</Text>)
    ```
    
    * Added test for wrapping JSX-statements
    
    * Fixed styling of JSX
    
    * Changed wrap to either accept a React component or a function
    
    Uses ReactComponent.prototype.isReactComponent = {} to check, whether the passed in argument is indeed a React component or assume a function otherwise. Added a wrapping test for an old school React.createClass as well
    
    * Changed React Component test to Component.isValidElement
    
    * Fixed linting errors

--- src/reactWrapper.js
+++ src/reactWrapper.js
@@ -1,97 +1,26 @@
-import React from "react";
-import _ from "lodash";
-import { styles } from "./index";
-import cssColors from "css-color-names"
+import React from 'react';
+import _ from 'lodash';
+import { styles } from './index';
+import cssColors from 'css-color-names'
 
-export function wrap(WrappedComponent) {
+/* wrap takes a Component or a render function and recursively replaces
+   the prop 'cls' with the respective 'style' definitions.
+   Usually, wrapping a whole Class / Component will do the trick,
+   but for some render functions (e.g. ListView -> renderHeader)
+   this will not work. Hence the such functions need to be wrapped
+   individually */
+export function wrap(componentOrFunction) {
+    if (typeof componentOrFunction.prototype.isReactComponent !== 'object') {
+        const func = componentOrFunction;
+
+        return function wrappedRender(...args) {
+            return recursiveStyle(func.apply(this, args))
+        };
+    }
+    const WrappedComponent = componentOrFunction;
     const newClass = class extends WrappedComponent {
         render() {
-            return this._recursiveStyle(super.render());
-        }
-
-        _recursiveStyle(elementsTree) {
-            const { props } = elementsTree;
-            let newProps;
-            let translated = false;
-
-            /* Parse cls string */
-            if (_.isString(props.cls)) {
-                newProps = {}
-                translated = true
-                if (_.isArray(props.style)) {
-                    newProps.style = props.style.slice()
-
-                } else if (_.isObject(props.style)) {
-                    newProps.style = [props.style]
-
-                } else {
-                    newProps.style = []
-                }
-
-                const splitted = props.cls.replace(/-/g, "_").split(" ")
-                for (let i = 0; i < splitted.length; i++) {
-                    const cls = splitted[i];
-                    if (cls.length > 0) {
-                        const style = styles[cls];
-                        if (style) {
-
-                            /* Style found */
-                            newProps.style.push(style);
-
-                        } else if (cls.startsWith("bg_")) {
-                            newProps.style.push({
-                                backgroundColor: cls.slice(3)
-                            })
-
-                        } else if (cls.startsWith("b__")) {
-                            newProps.style.push({
-                                borderColor: cls.slice(3)
-                            })
-
-                        } else if (cssColors[cls] || (/^(rgb|#|hsl)/).test(cls)) {
-                            newProps.style.push({
-                                color: cls
-                            })
-
-                        } else {
-                            throw new Error(`style '${cls}' not found`);
-                        }
-
-                    }
-                }
-            }
-
-            let newChildren = props.children;
-            if (_.isArray(newChildren)) {
-
-                /* Convert child array */
-                newChildren = React.Children.toArray(newChildren);
-                for (let i = 0; i < newChildren.length; i++) {
-                    const c = newChildren[i];
-                    if (React.isValidElement(c)) {
-                        const converted = this._recursiveStyle(c);
-                        if (converted !== c) {
-                            translated = true;
-                            newChildren[i] = converted;
-                        }
-                    }
-                }
-
-            } else if (React.isValidElement(newChildren)) {
-
-                /* Convert single child */
-                const converted = this._recursiveStyle(newChildren);
-                if (converted !== newChildren) {
-                    translated = true;
-                    newChildren = converted;
-                }
-            }
-
-            if (translated) {
-                return React.cloneElement(elementsTree, newProps, newChildren)
-            }
-
-            return elementsTree;
+            return recursiveStyle(super.render());
         }
     }
 
@@ -101,3 +30,87 @@ export function wrap(WrappedComponent) {
     return newClass;
 }
 
+function recursiveStyle(elementsTree) {
+    const { props } = elementsTree;
+    let newProps;
+    let translated = false;
+
+    /* Parse cls string */
+    if (_.isString(props.cls)) {
+        newProps = {}
+        translated = true
+        if (_.isArray(props.style)) {
+            newProps.style = props.style.slice()
+
+        } else if (_.isObject(props.style)) {
+            newProps.style = [props.style]
+
+        } else {
+            newProps.style = []
+        }
+
+        const splitted = props.cls.replace(/-/g, '_').split(' ')
+        for (let i = 0; i < splitted.length; i++) {
+            const cls = splitted[i];
+            if (cls.length > 0) {
+                const style = styles[cls];
+                if (style) {
+
+                    /* Style found */
+                    newProps.style.push(style);
+
+                } else if (cls.startsWith('bg_')) {
+                    newProps.style.push({
+                        backgroundColor: cls.slice(3)
+                    })
+
+                } else if (cls.startsWith('b__')) {
+                    newProps.style.push({
+                        borderColor: cls.slice(3)
+                    })
+
+                } else if (cssColors[cls] || (/^(rgb|#|hsl)/).test(cls)) {
+                    newProps.style.push({
+                        color: cls
+                    })
+
+                } else {
+                    throw new Error(`style '${cls}' not found`);
+                }
+
+            }
+        }
+    }
+
+    let newChildren = props.children;
+    if (_.isArray(newChildren)) {
+
+        /* Convert child array */
+        newChildren = React.Children.toArray(newChildren);
+        for (let i = 0; i < newChildren.length; i++) {
+            const c = newChildren[i];
+            if (React.isValidElement(c)) {
+                const converted = recursiveStyle(c);
+                if (converted !== c) {
+                    translated = true;
+                    newChildren[i] = converted;
+                }
+            }
+        }
+
+    } else if (React.isValidElement(newChildren)) {
+
+        /* Convert single child */
+        const converted = recursiveStyle(newChildren);
+        if (converted !== newChildren) {
+            translated = true;
+            newChildren = converted;
+        }
+    }
+
+    if (translated) {
+        return React.cloneElement(elementsTree, newProps, newChildren)
+    }
+
+    return elementsTree;
+}
