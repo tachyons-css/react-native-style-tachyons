8ee9151eca9617f2dccb7843c938257acd01fc76
Luang Chuenjit <ntpat.ch@gmail.com>
2017-05-15 15:50:18 +0700

    Improve code quality

--- src/styles/tracked.js
+++ src/styles/tracked.js
@@ -1,9 +1,9 @@
 import _ from "lodash";
 
 const scale = {
-    "tracked": .1,
-    "tracked-tight": -.05,
-    "tracked-mega": .25,
+    tracked: 0.1,
+    "tracked-tight": -0.05,
+    "tracked-mega": 0.25,
 }
 
 export default _.mapValues(scale, val => ({ letterSpacing: val }))
